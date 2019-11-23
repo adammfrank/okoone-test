7c1d2ea87d568d50f64a19466f15e12e31885ed7
Adam Frank <adamfrank@Adams-MacBook-Pro.local>
2019-11-23 16:39:12 +0700

    callbacks to async/await in controller

--- api/controllers/article.js
+++ api/controllers/article.js
@@ -9,7 +9,7 @@ var sendJSONresponse = function (res, status, content) {
     res.json(content);
 };
 
-module.exports.store = function (req, res) {
+module.exports.store = async function (req, res) {
 
     const article = new Article();
 
@@ -17,16 +17,17 @@ module.exports.store = function (req, res) {
     article.description = req.body.description;
     article.date = req.body.date;
 
-    article.save(function (err) {
-        if (err) {
-            return res.status(400).json(err);
-        }
+    try {
+        await article.save()
+        return res.status(200).json(article);
+    }
+    catch (err) {
+        return res.status(400).json(err);
+    }
 
-        res.status(200).json(article);
-    });
 };
 
-module.exports.list = function (req, res) {
+module.exports.list = async function (req, res) {
 
     const sort = req.query.sort === 'desc' ? -1 : 1;
 
@@ -38,26 +39,26 @@ module.exports.list = function (req, res) {
         query['date'] = { '$lt': req.query.date };
     }
 
-    Article.find(query)
-        .sort({ 'title': sort }).exec(function (err, articles) {
-            if (err) {
-                return res.status(400).json(err);
-            }
+    try {
+        const articles = await Article.find(query)
+            .sort({ 'title': sort }).exec();
+        res.status(200).json(articles);
+    }
+    catch (err) {
+        return res.status(400).json(err);
+    }
 
-            res.status(200).json(articles);
-
-        });
 };
 
-module.exports.get = function (req, res) {
+module.exports.get = async function (req, res) {
     const _id = req.params._id;
-    Article.find({ '_id': _id }, function (err, article) {
-        if (err) {
-            return res.status(400).json(err);
-        }
-
-        res.status(200).json(article);
-    });
+    try {
+        const article = await Article.find({ '_id': _id });
+        return res.status(200).json(article);
+    }
+    catch (err) {
+        return res.status(400).json(err);
+    }
 }
 
 module.exports.update = async function (req, res) {
